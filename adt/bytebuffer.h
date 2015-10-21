9e0943d0bd28eaf5f6b79bbf4d1ce38667679637
Alexander Koch <kochalexander@gmx.net>
2015-10-21 21:11:36 +0200

    Add bytebuffer files

---
+++ adt/bytebuffer.h
@@ -0,0 +1,19 @@
+#ifndef bytebuffer_h
+#define bytebuffer_h
+
+#include <stdint.h>
+#include <core/mem.h>
+
+typedef struct
+{
+	uint8_t* data;
+	int count;
+	int capacity;
+} bytebuffer_t;
+
+void bytebuffer_init(bytebuffer_t* buffer);
+void bytebuffer_clear(bytebuffer_t* buffer);
+void bytebuffer_fill(bytebuffer_t* buffer, uint8_t data, int count);
+void bytebuffer_write(bytebuffer_t* buffer, uint8_t data);
+
+#endif
