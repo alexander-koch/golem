ac297454e28129f364b60180d2fa2bea47d39c60
Alexander Koch <kochalexander@gmx.net>
2016-02-29 20:15:21 +0100

    Add label support for the assembler

--- scripts/assembler.py
+++ scripts/assembler.py
@@ -35,14 +35,39 @@ opcodes = {
 	'imul': (b'\x0B', b'\x00'),
 	'idiv': (b'\x0C', b'\x00'),
 	'mod': (b'\x0D', b'\x00'),
-	'syscall': (b'\x1E', b'\x01')
+	'bitl': (b'\x0E', b'\x00'),
+	'bitr': (b'\x0F', b'\x00'),
+	'bitand': (b'\x10', b'\x00'),
+	'bitor': (b'\x11', b'\x00'),
+	'bitxor': (b'\x12', b'\x00'),
+	'bitnot': (b'\x13', b'\x00'),
+	'iminus': (b'\x14', b'\x00'),
+	'i2f': (b'\x15', b'\x00'),
+	'fadd': (b'\x16', b'\x00'),
+	'fsub': (b'\x17', b'\x00'),
+	'fmul': (b'\x18', b'\x00'),
+	'fdiv': (b'\x19', b'\x00'),
+	'fminus': (b'\x1A', b'\x00'),
+	'f2i': (b'\x1B', b'\x00'),
+	'not': (b'\x1C', b'\x00'),
+	'b2i': (b'\x1D', b'\x00'),
+	'syscall': (b'\x1E', b'\x01'),
+	'invoke': (b'\x1F', b'\x02'),
+	'invokevirtual': (b'\x20', b'\x02'),
+	'reserve': (b'\x21', b'\x01'),
+	'ret': (b'\x22', b'\x00')
 }
 
-def writeValue(bytecode, arg):
-	if arg.isdigit():
-		bytecode += b'\x01'
-		bytecode += struct.pack("<q", int(arg))
+def check_int(s):
+    if s[0] in ('-', '+'):
+    	return s[1:].isdigit()
+    return s.isdigit()
 
+def writeValue(bytecode, arg):
+	if check_int(arg):
+		bytecode += b'\x01'
+		bytecode += struct.pack("<i", int(arg))
+		bytecode += struct.pack("<i", 0)
 	elif arg == "true" or arg == "false":
 		bytecode += b'\x02'
 		bval = (arg == "true")
@@ -61,6 +86,15 @@ def main():
 	# Write the header
 	bytecode += b'\x55\xCE\xAC\x00'
 	bytecode += b'\x00\x00\x00\x00'
+
+	# Initial jump
+	bytecode += b'\x24'
+	bytecode += b'\x01'
+	bytecode += b'\x01'
+	bytecode += struct.pack("<q", 0)
+	bytecode_count = bytecode_count + 1
+
+	labels = {}
 
 	# Begin decoding the file
 	with open(sys.argv[1],'r') as f:
@@ -81,9 +115,39 @@ def main():
 
 			# Get the opcode
 			opcode = tokens[0]
-			if opcode[len(opcode)-1] == ',':
-				opcode = opcode[0:-1]
 
+			# Remove trailing comma
+			if opcode[-1:] == ',':
+				opcode = opcode[:-1]
+
+			# Test for labels
+			elif opcode[-1:] == ':':
+				# Get the label name
+				label = opcode[:-1]
+				labels[label] = bytecode_count
+				continue
+
+			# Test for call
+			if opcode == "call":
+				label = args[0]
+				if label in labels:
+					# invoke
+					bytecode += b'\x1F'
+					bytecode += b'\x02'
+
+					# Arg1
+					bytecode += b'\x01'
+					bytecode += struct.pack("<q", labels[label])
+					bytecode_count = bytecode_count + 1
+
+					# Arg2
+					argc = int(args[1])
+					bytecode += b'\x01'
+					bytecode += struct.pack("<q", argc)
+					bytecode_count = bytecode_count + 1
+				continue
+
+			# Write one instruction
 			if opcode in opcodes:
 				ituple = opcodes[opcode]
 				bytecode += ituple[0]
@@ -97,6 +161,16 @@ def main():
 				bytecode_count = bytecode_count + 1
 			else:
 				print("Opcode: "+opcode+" does not exist.")
+
+	print(labels)
+
+	# Testing for main
+	if "main" in labels:
+		# Set the initial jump
+		bytecode[11:11+8] = struct.pack("<q", labels["main"])
+	else:
+		print("Must provide a main label")
+		sys.exit()
 
 	# Set the number of bytecodes and write
 	bytecode[4:8] = struct.pack("<i", bytecode_count)
